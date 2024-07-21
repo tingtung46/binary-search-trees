61ede2a497fece0d54169b83f1fcd3241e1f3727
root <132725436+tingtung46@users.noreply.github.com>
2024-07-22 00:07:39 +0700

    Construct tree object and nuild tree method

---
+++ tree.js
@@ -0,0 +1,26 @@
+import Node from "./node.js";
+
+export default class Tree {
+  constructor() {
+    this.root = null;
+  }
+
+  buildTree(array) {
+    if (array.length < 1) return null;
+
+    const newArr = [...new Set(array)].sort();
+
+    let start = 0;
+    let end = newArr.length - 1;
+
+    const mid = Math.floor((start + end) / 2);
+    const node = new Node(newArr[mid]);
+
+    node.left = this.buildTree(newArr.slice(0, mid));
+    node.right = this.buildTree(newArr.slice(mid + 1));
+
+    this.root = node;
+
+    return this.root;
+  }
+}
